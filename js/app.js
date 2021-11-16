1cbb8cbca29c4a8725a10f0ceafba2a0abe3869d
Nurgul Amat <nurgulamat212@gmail.com>
2021-11-15 22:25:18 -0600

    added new message closing function and toggle switch

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-alertBanner = document.getElementById("alert");
+const alertBanner = document.getElementById("alert");
 // create the HTML for the banner
 alertBanner.innerHTML= `<div class="alert-banner">
                           <p><strong>Alert:</strong> You have unread messages</p>
@@ -9,4 +9,64 @@ alertBanner.addEventListener('click', e => {
     if(element.classList.contains("alert-banner-close")){
         alertBanner.style.display = "none";
     }
+});
+
+// Closing new messages
+const newMessage = document.getElementById("notificationList");
+newMessage.addEventListener('click', e =>{
+    const message = e.target;
+    if(message.classList.contains("close-message")){
+        message.parentNode.style.display = "none";
+    }
+
+});
+
+// Traffic Chart
+const trafficCanvas = document.getElementById("traffic-chart");
+const trafficData = {
+    labels: ["16-22", "23-29", "30-5", "6-12", "13-19", "20-26", "27-3","4-10", "11-17", "18-24", "25-31"],
+    datasets: [{
+        data: [750, 1250, 1000, 2000, 1500, 1750, 1250, 1850, 2250, 1500, 2500],
+        backgroundColor: 'rgba(116, 119, 191, .3)',
+        borderWidth: 1,
+    }]
+}
+
+
+
+
+
+// Doughnut chart
+const mobileCanvas = document.getElementById("mobile-chart");
+const mobileData = {
+    labels: ["Desktop", "Tablet", "Phones"],
+    datasets: [{
+        label: "# of Users",
+        data: [2000, 550, 500],
+        borderWidth: 0,
+        backgroundColor: [
+            '#7477BF',
+            '#78CF82',
+            '#51B6C8'
+        ]
+    }]
+};
+
+const mobileOptions = {
+    aspectRatio: 1.9,
+    plugins: {
+        legend: {
+            position: 'right',
+            labels: {
+                boxWidth: 20,
+                fontStyle: 'bold'
+            }
+        }
+    }
+};
+
+let mobileChart = new Chart(mobileCanvas, {
+    type: 'doughnut',
+    data: mobileData,
+    options: mobileOptions
 });
